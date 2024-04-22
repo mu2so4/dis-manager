4d713163340d7878ec24481e647bbe4bd224d96b
mu2so4 <b4.orangutang@gmail.com>
2024-04-22 23:42:36 +0700

    Initial commit

---
+++ src/test/java/ru/nsu/ccfit/muratov/distributed/crack/CrackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.muratov.distributed.crack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
