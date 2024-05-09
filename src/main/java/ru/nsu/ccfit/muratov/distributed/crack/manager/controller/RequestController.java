07243a7d8d2141cfad1cbcc9924ad9ebb5609331
mu2so4 <b4.orangutang@gmail.com>
2024-05-09 15:59:56 +0700

    Add supporting of API documentation in Swagger

--- src/main/java/ru/nsu/ccfit/muratov/distributed/crack/manager/controller/RequestController.java
+++ src/main/java/ru/nsu/ccfit/muratov/distributed/crack/manager/controller/RequestController.java
@@ -1,5 +1,10 @@
 package ru.nsu.ccfit.muratov.distributed.crack.manager.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +22,7 @@ import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/api/hash")
+@Tag(name = "Crack requests")
 public class RequestController {
     @Autowired
     private CrackService service;
@@ -26,7 +32,20 @@ public class RequestController {
 
     private static final Logger logger = Logger.getLogger(RequestController.class.getCanonicalName());
 
+    @Operation(
+            summary = "Create a new request to crack an MD5 hash",
+            description = "Creates a new request to crack an MD5 hash. " +
+                    "Note if there is a request with the same hash and equal or greater max length, " +
+                    "the ID of that request will be returned."
+    )
+    @ApiResponses(
+            @ApiResponse(
+                    description = "Success",
+                    responseCode = "201"
+            )
+    )
     @PostMapping(value = "/crack", consumes = "application/json", produces = "application/json")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public CrackResponseDto createRequest(@RequestBody CrackRequestDto request) {
         logger.info("got request " + request.toString());
         String hash = request.getHash();
@@ -43,7 +62,25 @@ public class RequestController {
     }
 
 
-    @GetMapping(value = "/status", consumes = "application/json", produces = "application/json")
+    @Operation(
+            summary = "Get status of the crack request",
+            description = "Gets status of the crack request by its ID"
+    )
+    @Parameter(
+            name = "requestId",
+            description = "ID of request created"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Success",
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    description = "No request found",
+                    responseCode = "404"
+            )
+    })
+    @GetMapping(value = "/status", produces = "application/json")
     public StatusDto getStatus(@RequestParam(name="requestId") String id) {
         Request request = service.getRequest(id);
         if(request == null) {
@@ -58,8 +95,19 @@ public class RequestController {
         return dto;
     }
 
-    @DeleteMapping(value = "/crack")
-    public void deleteDatabase() {
+    @Operation(
+            summary = "Delete all request from the database",
+            description = "Deletes all request from the database except in progress ones. This method is used in development purposes."
+    )
+    @ApiResponse(
+            description = "Success",
+            responseCode = "204"
+    )
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/crack", produces = "text/plain")
+    public String deleteDatabase() {
         service.deleteAll();
+        logger.info("DB data cleared");
+        return "Database data were deleted!";
     }
 }
--- src/main/java/ru/nsu/ccfit/muratov/distributed/crack/manager/dto/CrackRequestDto.java
+++ src/main/java/ru/nsu/ccfit/muratov/distributed/crack/manager/dto/CrackRequestDto.java
@@ -1,9 +1,15 @@
 package ru.nsu.ccfit.muratov.distributed.crack.manager.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CrackRequestDto {
+    @Schema(description = "hash to be cracked", example = "1edd5f66067229b7d2411cb1658074d6")
     private String hash;
+
+
+
+    @Schema(description = "max length of words which may match the hash", example = "4")
     private int maxLength;
 }
